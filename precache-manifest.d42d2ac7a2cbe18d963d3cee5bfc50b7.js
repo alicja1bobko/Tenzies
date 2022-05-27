9a6a2c72a4191aa4e5f2b208a9923e008014beb3
AlicjaBobko <alicja_bobko@o2.pl>
2022-05-27 12:03:43 +0200

    Updates

---
+++ precache-manifest.d42d2ac7a2cbe18d963d3cee5bfc50b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e7a37302054db917aa8",
+    "url": "/Tenzies/static/css/main.296c1db0.chunk.css"
+  },
+  {
+    "revision": "5e7a37302054db917aa8",
+    "url": "/Tenzies/static/js/main.5e7a3730.chunk.js"
+  },
+  {
+    "revision": "e34f04b9aeec176f77a7",
+    "url": "/Tenzies/static/js/1.e34f04b9.chunk.js"
+  },
+  {
+    "revision": "3acee836ea9f9c6bc2bb",
+    "url": "/Tenzies/static/js/runtime~main.3acee836.js"
+  },
+  {
+    "revision": "c55fa852e3275e7cc9dc1845cde10e3e",
+    "url": "/Tenzies/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Tenzies/precache-manifest.d42d2ac7a2cbe18d963d3cee5bfc50b7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Tenzies/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
